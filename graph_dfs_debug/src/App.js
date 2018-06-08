02c68043a3ef33f026648c144fee5259a9a9d4fc
William Pelton <williamrpelton@gmail.com>
2018-06-08 15:21:52 -0500

    William Pelton

--- graph_dfs_debug/src/App.js
+++ graph_dfs_debug/src/App.js
@@ -59,7 +59,7 @@ class GraphView extends Component {
     }
 
     // Draw the verts on top
-    ctx.fillStyle = '#77f'; 
+    ctx.fillStyle = color; 
 
     for (let v of vertexes) {
       ctx.beginPath();
@@ -155,7 +155,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.Button}>Random</button>
+        <button onClick={this.onButton}>Random</button>
         <GraphView graph={this.state.graph}></GraphView>
       </div>
     );
